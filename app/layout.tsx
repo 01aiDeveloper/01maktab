95261d5a049521894f0bf713d6c43e421b1d6ffe
MuhammadqodirProgrammer <komilovm831@gmail.com>
2026-01-06 22:34:08 +0500

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,19 +8,11 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "01 Maktab",
+  description: "01 Maktab",
+  generator: "01 Maktab",
   icons: {
     icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
       {
         url: "/icon.svg",
         type: "image/svg+xml",
